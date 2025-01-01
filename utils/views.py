a54a401315cb278606bf421873bc8a9d832ddc3e
Amber <amber@transcental.dev>
2025-01-01 14:42:23 +0000

    oh shit, the old domain is still here

--- utils/views.py
+++ utils/views.py
@@ -45,7 +45,7 @@ def generate_home_view(
                                 "emoji": True,
                             },
                             "style": "primary",
-                            "url": "https://slickstats.dillonb07.studio/slack/install",
+                            "url": "https://slickstats.transcental.dev/slack/install",
                             "action_id": "authorise-btn",
                         }
                     ],
